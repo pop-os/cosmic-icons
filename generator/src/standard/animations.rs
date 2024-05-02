32f113f29f2a8eefac26ce105a247388a48a6e36
Jeremy Soller <jackpot51@gmail.com>
2024-05-02 09:29:45 -0600

    Add generator for checking for missing icons

---
+++ generator/src/standard/animations.rs
@@ -0,0 +1,4 @@
+pub static CONTEXT: &'static str = "Animations";
+pub static ICONS: &'static [&'static str] = &[
+    "process-working", // This is the standard spinner animation for web browsers and file managers to show that the location is loading.
+];
---
+++ generator/src/standard/places.rs
@@ -0,0 +1,12 @@
+pub static CONTEXT: &'static str = "Places";
+pub static ICONS: &'static [&'static str] = &[
+    "folder", // The standard folder icon used to represent directories on local filesystems, mail folders, and other hierarchical groups.
+    "folder-remote", // The icon used for normal directories on a remote filesystem.
+    "network-server", // The icon used for individual host machines under the \u201cNetwork Servers\u201d place in the file manager.
+    "network-workgroup", // The icon for the \u201cNetwork Servers\u201d place in the desktop's file manager, and workgroups within the network.
+    "start-here", // The icon used by the desktop's main menu for accessing places, applications, and other features.
+    "user-bookmarks", // The icon for the user's special \u201cBookmarks\u201d place.
+    "user-desktop", // The icon for the special \u201cDesktop\u201d directory of the user.
+    "user-home",  // The icon for the special \u201cHome\u201d directory of the user.
+    "user-trash", // The icon for the user's \u201cTrash\u201d place in the desktop's file manager.
+];
